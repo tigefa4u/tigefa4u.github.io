00fc2d4e6b687ab04684f9a62a6589320389d04c
sugeng tigefa <tigefa@gmail.com>
2013-09-14 09:02:17 +0700

    Update content

---
+++ _plugins/conventer.rb
@@ -0,0 +1,18 @@
+module Jekyll
+  class UpcaseConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /^\.upcase$/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      content.upcase
+    end
+  end
+end
